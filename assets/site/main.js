d39f132f82e9e642e7ce60adba4f13e6b832babf
Riley Brown <riley@riley.gg>
2019-07-26 23:02:09 -0400

    added function for toggle menu

---
+++ assets/site/main.js
@@ -0,0 +1,18 @@
+function toggleSeasonsMenu() {
+  const seasonsMenu = document.querySelector('.seasons-menu');
+  const seasonsNav = document.querySelector('#seasons button');
+  const seasonsNavImg = document.querySelector('#seasons button img');
+
+  seasonsNav.addEventListener('click', e => {
+    seasonsMenu.classList.toggle('seasons-menu-active');
+    seasonsNav.classList.toggle('seasons-nav-active');
+
+    if (seasonsNav.className === 'seasons-nav-active') {
+      seasonsNavImg.src = './assets/svg/icon-opened-caret.svg';
+    } else {
+      seasonsNavImg.src = './assets/svg/icon-closed-caret.svg';
+    }
+  });
+}
+
+toggleSeasonsMenu();
