0a7f7e72360a24b61a39bffaf1b5b465fc3b33fa
Riley Brown <riley@riley.gg>
2019-07-27 22:30:59 -0400

    added mobile nav toggle logic

--- assets/site/main.js
+++ assets/site/main.js
@@ -16,3 +16,26 @@ function toggleSeasonsMenu() {
 }
 
 toggleSeasonsMenu();
+
+function toggleMobileMenu() {
+  const menuToggleNav = document.querySelector('.menu-toggle');
+  const mobileMenu = document.querySelector('.header-container');
+  const menuToggleButton = document.querySelector('.menu-toggle button');
+  const menuToggleIcon = document.querySelector('.menu-toggle img');
+
+  menuToggleNav.addEventListener('click', e => {
+    mobileMenu.classList.toggle('mobile-menu-active');
+
+    if (mobileMenu.className.includes('active')) {
+      menuToggleButton.innerText = 'Close';
+      menuToggleButton.style.color = 'var(--text-white)';
+      menuToggleIcon.src = './assets/svg/icon-opened-caret.svg';
+    } else {
+      menuToggleButton.innerText = 'Menu';
+      menuToggleButton.style.color = 'var(--nav-links)';
+      menuToggleIcon.src = './assets/svg/icon-closed-caret.svg';
+    }
+  });
+}
+
+toggleMobileMenu();
